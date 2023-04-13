6db82b0283f0c5f9a5b485947c6988e07a828ead
Parinav-Urade-Fujitsu <parinav.urade@fujitsu.com>
2023-04-13 22:25:52 +0530

    T017 Completed
    Added TraineeRecordsPage (T017)
    and added did basic controller parts for "login "page

---
+++ TrainingStatusApp/src/main/java/com/Spring/TrainingStatusApp/Controller/LoginController.java
@@ -0,0 +1,14 @@
+package com.Spring.TrainingStatusApp.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class LoginController {
+	
+	@RequestMapping(value = "/login", method =  RequestMethod.GET)
+	public String loginPage() {
+		return "login";
+	}
+}
--- TrainingStatusApp/src/main/java/com/Spring/TrainingStatusApp/TrainingStatusAppApplication.java
+++ TrainingStatusApp/src/main/java/com/Spring/TrainingStatusApp/TrainingStatusAppApplication.java
@@ -2,8 +2,13 @@ package com.Spring.TrainingStatusApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+
+@ComponentScan("com.Spring.TrainingStatusApp")
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+
 public class TrainingStatusAppApplication {
 
 	public static void main(String[] args) {
